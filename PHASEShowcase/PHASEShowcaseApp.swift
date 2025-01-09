43e727aa39cdfe81438e3de87dadc8e668230377
Adam Czyzak <adam.czyzzak@gmail.com>
2025-01-09 12:39:58 +0100

    Initial Commit

---
+++ PHASEShowcase/PHASEShowcaseApp.swift
@@ -0,0 +1,17 @@
+//
+//  PHASEShowcaseApp.swift
+//  PHASEShowcase
+//
+//  Created by Adam Czyżak on 09/01/2025.
+//
+
+import SwiftUI
+
+@main
+struct PHASEShowcaseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
